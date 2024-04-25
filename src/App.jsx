a101f0c119d0a6b0166dbc05c4eaed902d86b0df
gabrieltkoti <165932384+gabrieltkoti@users.noreply.github.com>
2024-04-25 22:26:50 +0000

    Implementação dos Cards como componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+  }
+
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+  }
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+  }
+
+  const itens = [item1, item2, item3]
+
   return (
     <>
-      <div className='card'> 
-        <h2 className='titleName'> Rick Sanches </h2>
-        <div className='info'>
-          <tag>Informação 1</tag>
-          <tag>Informação 2</tag>
-          <tag>Informação 3</tag>
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      {itens.map(function (elemento) {
+        return <Card item={elemento} />
+      })}
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+export default function Card (props){
+    const item = props.item
+
+    return (
+        <div className='card'> 
+            <h2>{item.name}</h2>
+            <div className='info'>
+                <div>Status: Vivo</div>
+                <div>Espécie: Humana</div>
+                <div>Origem: Terra C-137</div>
+            </div>
+            <img src={item.image} />
+        </div>
+    )
+}
